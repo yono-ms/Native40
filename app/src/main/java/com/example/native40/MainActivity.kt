5a4e5db77ef5e5f72df77f9c92a7898671a429d5
yono-ms <yono.ms@outlook.com>
2020-06-20 10:23:13 +0900

    slf4j

--- app/src/main/java/com/example/native40/MainActivity.kt
+++ app/src/main/java/com/example/native40/MainActivity.kt
@@ -1,11 +1,23 @@
+/*
+ * Copyright (c) 2020. yono-ms
+ */
+
 package com.example.native40
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        private val logger: Logger = LoggerFactory.getLogger(MainActivity::class.java.simpleName)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        logger.info("onCreate savedInstanceState=$savedInstanceState")
         setContentView(R.layout.activity_main)
     }
 }
