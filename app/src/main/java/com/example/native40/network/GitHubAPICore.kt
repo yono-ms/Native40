c81600d78f5ac862c07a6bbafbf863cd5b703715
yono-ms <yono.ms@outlook.com>
2020-07-29 22:02:46 +0900

    enable plane text log. (#5)

--- app/src/main/java/com/example/native40/network/GitHubAPICore.kt
+++ app/src/main/java/com/example/native40/network/GitHubAPICore.kt
@@ -6,8 +6,7 @@ package com.example.native40.network
 
 import com.example.native40.network.model.GitHubUserModel
 import com.github.kittinunf.fuel.Fuel
-import com.github.kittinunf.fuel.coroutines.awaitObjectResponseResult
-import com.github.kittinunf.fuel.serialization.kotlinxDeserializerOf
+import com.github.kittinunf.fuel.coroutines.awaitStringResponseResult
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.json.JsonArray
 import kotlinx.serialization.json.JsonArraySerializer
@@ -27,24 +26,19 @@ abstract class GitHubAPICore {
 
     suspend fun getUsers(login: String): GitHubUserModel {
         // Fuel default scope is IO
-        val (request, response, result) = Fuel.get("$url_users$login")
-            .awaitObjectResponseResult(
-                kotlinxDeserializerOf(GitHubUserModel.serializer(), json)
-            )
+        val (request, response, result) = Fuel.get("$url_users$login").awaitStringResponseResult()
         log("$request")
         log("$response")
-        log(result.get().toString())
-        return result.get()
+        val text = result.get()
+        return json.parse(GitHubUserModel.serializer(), text)
     }
 
     suspend fun getRepos(login: String): JsonArray {
         val (request, response, result) = Fuel.get("$url_users$login/repos")
-            .awaitObjectResponseResult(
-                kotlinxDeserializerOf(JsonArraySerializer, json)
-            )
+            .awaitStringResponseResult()
         log("$request")
         log("$response")
-        log(result.get().toString())
-        return result.get()
+        val text = result.get()
+        return json.parse(JsonArraySerializer, text)
     }
 }
