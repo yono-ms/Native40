6aa08ba8a9c70d7c5965195ab69d59a135637aa7
yono-ms <yono.ms@outlook.com>
2020-08-03 23:49:07 +0900

    refactor RecyclerView binding.

--- app/src/main/java/com/example/native40/HistoryFragment.kt
+++ app/src/main/java/com/example/native40/HistoryFragment.kt
@@ -10,7 +10,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
@@ -19,9 +18,8 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.example.native40.databinding.HistoryFragmentBinding
+import com.example.native40.databinding.UserItemBinding
 import com.example.native40.db.User
-import java.text.SimpleDateFormat
-import java.util.*
 
 class HistoryFragment : BaseFragment() {
 
@@ -48,16 +46,12 @@ class HistoryFragment : BaseFragment() {
             it.lifecycleOwner = viewLifecycleOwner
             initBaseFragment(viewModel)
             it.recyclerView.layoutManager = LinearLayoutManager(context)
-            it.recyclerView.adapter = UserAdapter(object : DiffUtil.ItemCallback<User>() {
-                override fun areItemsTheSame(oldItem: User, newItem: User): Boolean {
-                    return oldItem.id == newItem.id
-                }
-
-                override fun areContentsTheSame(oldItem: User, newItem: User): Boolean {
-                    return oldItem == newItem
-                }
-            }, object : OnAdapterClickListener {
-                override fun onClick(login: String) {
+            it.recyclerView.adapter = UserAdapter().also { adapter ->
+                viewModel.items.observe(viewLifecycleOwner, Observer { items ->
+                    logger.info("viewModel.items changed. ${items?.size}")
+                    adapter.submitList(items)
+                })
+                adapter.setOnClickListener { login ->
                     logger.info("onClick $login")
                     viewModel.dialogMessage.value = DialogMessage(
                         RequestCode.OK_CANCEL,
@@ -66,11 +60,6 @@ class HistoryFragment : BaseFragment() {
                         listOf(login)
                     )
                 }
-            }).also { adapter ->
-                viewModel.items.observe(viewLifecycleOwner, Observer { items ->
-                    logger.info("viewModel.items changed. ${items?.size}")
-                    adapter.submitList(items)
-                })
             }
         }
         return binding.root
@@ -89,38 +78,42 @@ class HistoryFragment : BaseFragment() {
         }
     }
 
-    interface OnAdapterClickListener {
-        fun onClick(login: String)
-    }
+    class UserAdapter :
+        ListAdapter<User, UserAdapter.ViewHolder>(object : DiffUtil.ItemCallback<User>() {
+            override fun areItemsTheSame(oldItem: User, newItem: User): Boolean {
+                return oldItem.id == newItem.id
+            }
 
-    class UserAdapter(
-        diffCallback: DiffUtil.ItemCallback<User>,
-        private val listener: OnAdapterClickListener
-    ) :
-        ListAdapter<User, UserAdapter.ViewHolder>(diffCallback) {
-        class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-            val text1: TextView = itemView.findViewById(android.R.id.text1)
-            val text2: TextView = itemView.findViewById(android.R.id.text2)
-        }
+            override fun areContentsTheSame(oldItem: User, newItem: User): Boolean {
+                return oldItem == newItem
+            }
+        }) {
+
+        class ViewHolder(val binding: UserItemBinding) : RecyclerView.ViewHolder(binding.root)
 
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-            return ViewHolder(
-                LayoutInflater.from(parent.context)
-                    .inflate(android.R.layout.simple_list_item_2, parent, false)
-            ).also { viewHolder ->
-                viewHolder.itemView.setOnClickListener {
-                    val text = it.findViewById<TextView>(android.R.id.text1).text.toString()
-                    listener.onClick(text)
+            val binding = DataBindingUtil.inflate<UserItemBinding>(
+                LayoutInflater.from(parent.context),
+                R.layout.user_item,
+                parent,
+                false
+            )
+            return ViewHolder(binding).also {
+                it.itemView.setOnClickListener {
+                    val text = binding.user?.login ?: "???"
+                    onClick(text)
                 }
             }
         }
 
         override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-            holder.text1.text = getItem(position).login
-            holder.text2.text = SimpleDateFormat(
-                "yyyy/MM/dd HH:mm",
-                Locale.getDefault()
-            ).format(getItem(position).timeStamp)
+            holder.binding.user = getItem(position)
+        }
+
+        lateinit var onClick: (login: String) -> Unit
+
+        fun setOnClickListener(listener: (login: String) -> Unit) {
+            this.onClick = listener
         }
     }
 }
