51effe3cbc81b974a9c2e1597f56d2100a58affc
yono-ms <yono.ms@outlook.com>
2020-08-04 23:11:03 +0900

    refactor Home RecyclerView binding.

--- app/src/main/java/com/example/native40/HomeFragment.kt
+++ app/src/main/java/com/example/native40/HomeFragment.kt
@@ -9,7 +9,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
@@ -18,6 +17,7 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.example.native40.databinding.HomeFragmentBinding
+import com.example.native40.databinding.RepositoryItemBinding
 
 class HomeFragment : BaseFragment() {
 
@@ -52,22 +52,7 @@ class HomeFragment : BaseFragment() {
                 viewModel.destination.value = Destination.PUSH_HISTORY
             }
             it.recyclerView.layoutManager = LinearLayoutManager(context)
-            it.recyclerView.adapter = PairAdapter(object :
-                DiffUtil.ItemCallback<Pair<String, String>>() {
-                override fun areItemsTheSame(
-                    oldItem: Pair<String, String>,
-                    newItem: Pair<String, String>
-                ): Boolean {
-                    return oldItem.first == newItem.first
-                }
-
-                override fun areContentsTheSame(
-                    oldItem: Pair<String, String>,
-                    newItem: Pair<String, String>
-                ): Boolean {
-                    return oldItem == newItem
-                }
-            }).also { adapter ->
+            it.recyclerView.adapter = RepositoryAdapter().also { adapter ->
                 viewModel.items.observe(viewLifecycleOwner, Observer { items ->
                     logger.info("viewModel.items changed.")
                     adapter.submitList(items)
@@ -89,24 +74,38 @@ class HomeFragment : BaseFragment() {
         }
     }
 
-    class PairAdapter(diffCallback: DiffUtil.ItemCallback<Pair<String, String>>) :
-        ListAdapter<Pair<String, String>, PairAdapter.ViewHolder>(diffCallback) {
+    class RepositoryAdapter :
+        ListAdapter<HomeViewModel.Repository, RepositoryAdapter.ViewHolder>(object :
+            DiffUtil.ItemCallback<HomeViewModel.Repository>() {
+            override fun areItemsTheSame(
+                oldItem: HomeViewModel.Repository,
+                newItem: HomeViewModel.Repository
+            ): Boolean {
+                return oldItem.name == newItem.name
+            }
 
-        class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-            val text1: TextView = itemView.findViewById(android.R.id.text1)
-            val text2: TextView = itemView.findViewById(android.R.id.text2)
-        }
+            override fun areContentsTheSame(
+                oldItem: HomeViewModel.Repository,
+                newItem: HomeViewModel.Repository
+            ): Boolean {
+                return oldItem == newItem
+            }
+        }) {
+
+        class ViewHolder(val binding: RepositoryItemBinding) : RecyclerView.ViewHolder(binding.root)
 
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-            return ViewHolder(
-                LayoutInflater.from(parent.context)
-                    .inflate(android.R.layout.simple_list_item_2, parent, false)
+            val binding = DataBindingUtil.inflate<RepositoryItemBinding>(
+                LayoutInflater.from(parent.context),
+                R.layout.repository_item,
+                parent,
+                false
             )
+            return ViewHolder(binding)
         }
 
         override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-            holder.text1.text = getItem(position).first
-            holder.text2.text = getItem(position).second
+            holder.binding.repository = getItem(position)
         }
     }
 }
