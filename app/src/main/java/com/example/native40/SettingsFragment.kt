a6c847d0219e367aac6bd681debce09b6eab7301
yono-ms <yono.ms@outlook.com>
2020-09-21 08:29:01 +0900

    validation check.

--- app/src/main/java/com/example/native40/SettingsFragment.kt
+++ app/src/main/java/com/example/native40/SettingsFragment.kt
@@ -5,15 +5,43 @@
 package com.example.native40
 
 import android.os.Bundle
+import android.text.InputType
+import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.activityViewModels
+import androidx.preference.EditTextPreference
 import androidx.preference.PreferenceFragmentCompat
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 
 class SettingsFragment : PreferenceFragmentCompat() {
+
+    companion object {
+        private const val PATTERN_USER_NAME = "^[ -~]+$"
+    }
+
+    val logger: Logger by lazy {
+        LoggerFactory.getLogger(javaClass.simpleName)
+    }
 
     private val mainViewModel by activityViewModels<MainViewModel>()
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.root_preferences, rootKey)
+        val userName: EditTextPreference? = findPreference(getString(R.string.preference_name_key))
+        userName?.setOnBindEditTextListener { editText ->
+            editText.inputType = InputType.TYPE_CLASS_TEXT + InputType.TYPE_TEXT_VARIATION_NORMAL
+            editText.addTextChangedListener { editable ->
+                logger.info("addTextChangedListener $editable")
+                editable?.let {
+                    val pattern = PATTERN_USER_NAME
+                    val matched = Regex(pattern).matches(it.toString())
+                    logger.info("matched=$matched")
+                    if (it.isNotEmpty() && !matched) {
+                        it.delete(it.length - 1, it.length)
+                    }
+                }
+            }
+        }
     }
 
     override fun onResume() {
